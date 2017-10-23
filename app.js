22cb16343e3f7ffec992f8a3909822c73810e20b
Richard Montgomery <rikermonty@gmail.com>
2017-10-23 15:04:36 -0700

    added location objects plus a method for random customers

--- app.js
+++ app.js
@@ -1,5 +1,52 @@
 'use strict';
 
+var pike = {
+  customerMin: 23,
+  customerMax: 65,
+  avg: 6.3,
+  hourlyCustomers: function(){
+    return Math.floor(Math.random() * (pike.customerMax - pike.customerMin)) + pike.customerMin;
+  }
+};
+
+var seaTac = {
+  customerMin: 3,
+  customerMax: 24,
+  avg: 1.2,
+  hourlyCustomers: function(){
+    return Math.floor(Math.random() * (seaTac.customerMax - seaTac.customerMin)) + seaTac.customerMin;
+  }
+};
+
+var seaCenter = {
+  customerMin: 11,
+  customerMax: 38,
+  avg: 3.7,
+  hourlyCustomers: function(){
+    return Math.floor(Math.random() * (seaCenter.customerMax - seaCenter.customerMin)) + seaCenter.customerMin;
+  }
+};
+
+var capHill = {
+  customerMin: 20,
+  customerMax: 38,
+  avg: 2.3,
+  hourlyCustomers: function(){
+    return Math.floor(Math.random() * (capHill.customerMax - pike.customerMin)) + pike.customerMin;
+  }
+};
+
+var alki = {
+  customerMin: 2,
+  customerMax: 16,
+  avg: 4.6,
+  hourlyCustomers: function(){
+    return Math.floor(Math.random() * (alki.customerMax - alki.customerMin)) + alki.customerMin;
+  }
+};
+
+
+
 
 
 
@@ -35,33 +82,3 @@ function domStuff(){
 
 domStuff();
 */
-
-var pike {
-  customerMin: 23,
-  customerMax: 65,
-  avg: 6.3
-}
-
-var seaTac {
-  customerMin: 3,
-  customerMax: 24,
-  avg: 1.2
-}
-
-var seaCenter {
-  customerMin: 11,
-  customerMax: 38,
-  avg: 3.7
-}
-
-var capHill {
-  customerMin: 20,
-  customerMax: 38,
-  avg: 2.3
-}
-
-var alki {
-  customerMin: 2,
-  customerMax: 16,
-  avg: 4.6
-}
