f1be3ae2d7a56b6c365e0eaf2a3039259e8f3887
Richard Montgomery <rikermonty@gmail.com>
2017-10-23 13:23:23 -0700

    I don't know what I'm doing

---
+++ app.js
@@ -0,0 +1,30 @@
+
+var pike {
+  customerMin: 23,
+  customerMax: 65,
+  avg: 6.3
+}
+
+var seaTac {
+  customerMin: 3,
+  customerMax: 24,
+  avg: 1.2
+}
+
+var seaCenter {
+  customerMin: 11,
+  customerMax: 38,
+  avg: 3.7
+}
+
+var capHill {
+  customerMin: 20,
+  customerMax: 38,
+  avg: 2.3
+}
+
+var alki {
+  customerMin: 2,
+  customerMax: 16,
+  avg: 4.6
+}
