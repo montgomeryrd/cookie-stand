c9c3aa070e38cd36b041402587c4fe642b5278ab
Richard Montgomery <rikermonty@gmail.com>
2017-10-25 18:24:50 -0700

    Almost done with Tuesday's Assignment

--- tuesday.js
+++ tuesday.js
@@ -1,79 +1,90 @@
 'use strict';
 
-var store = [];
-var hoursOperation = [];
-var salesAmounts = [];
-var totalSales = 0;
-
 //This is my object constructor
-function Stores(name, minCustomers, maxCustomers, avg, hoursOfOperation, salesAmounts, totalSales){
+function Stores(name, minCustomers, maxCustomers, avg){
   this.name = name;
   this.minCustomers = minCustomers;
   this.maxCustomers = maxCustomers;
   this.avg = avg;
   this.hoursOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-  this.salesAmounts = salesAmounts;
-  this.totalSales = totalSales;
-  Stores.prototype.hourlySales = function(){
-    var min = this.minCustomers;
-    var max = this.maxCustomers;
-    var avg = this.avg;
-    var salesAmounts = [];
-    var totalHourlySales;
-    for(var i = 0 ; i < this.hoursOperation.length ; i++) {
-      var hourlyCustomers = Math.floor(Math.random() * (max - min)) + min;
-      this.salesAmounts.push(Math.ceil(hourlyCustomers * avg));
-      this.totalHourlySales += (Math.ceil(hourlyCustomers * avg));
-      console.log(salesAmounts[i], 'cookies');
-    }
-    return [this.salesAmounts, this.totalHourlySales];
-  };
+};
+
+//My function that calculates projected sales per hour and total sales
+Stores.prototype.hourlySales = function(){
+  this.salesAmounts = [];
+  this.totalHourlySales = 0;
+  var min = this.minCustomers;
+  var max = this.maxCustomers;
+  var avg = this.avg;
+  var hourlyCustomers;
+  for(var i = 0 ; i < this.hoursOperation.length ; i++) {
+    hourlyCustomers = Math.floor(Math.random() * (max - min)) + min;
+    this.salesAmounts.push(Math.ceil(hourlyCustomers * avg));
+    this.totalHourlySales += (Math.ceil(hourlyCustomers * avg));
+    console.log(this.salesAmounts[i], 'cookies');
+  }
+  return [this.salesAmounts, this.totalHourlySales];
+};
+
+var table = document.getElementById('table_content');
+var data = [];
+
+//My function to push data into data array
+Stores.prototype.rows = function() {
+  for (var i = 0 ; i < this.salesAmounts.length ; i++){
+    data.push('<th>' + this.name + '</th>' +
+    '<td>' + this.salesAmounts[0] + '</td>' +
+    '<td>' + this.salesAmounts[1] + '</td>' +
+    '<td>' + this.salesAmounts[2] + '</td>' +
+    '<td>' + this.salesAmounts[3] + '</td>' +
+    '<td>' + this.salesAmounts[4] + '</td>' +
+    '<td>' + this.salesAmounts[5] + '</td>' +
+    '<td>' + this.salesAmounts[6] + '</td>' +
+    '<td>' + this.salesAmounts[7] + '</td>' +
+    '<td>' + this.salesAmounts[8] + '</td>' +
+    '<td>' + this.salesAmounts[9] + '</td>' +
+    '<td>' + this.salesAmounts[10] + '</td>' +
+    '<td>' + this.salesAmounts[11] + '</td>' +
+    '<td>' + this.salesAmounts[12] + '</td>' +
+    '<td>' + this.salesAmounts[13] + '</td>');
+  }
+
+  //append to html
+  var newRow;
+
+  for (var j = 0 ; j < 1 ; j++){
+    newRow = document.createElement('tr');
+    newRow.innerHTML = data[j];
+    table.appendChild(newRow);
+  }
 };
 
 //store locations
-var pike = new Stores('pike', 23, 65, 6.3, hoursOperation, salesAmounts, totalSales);
-var seaTac = new Stores('seaTac', 3, 24, 1.2, hoursOperation, salesAmounts, totalSales);
-var seaCenter = new Stores('seaCenter', 11, 38, 3.7, hoursOperation, salesAmounts, totalSales);
-var capHill = new Stores('capHill', 20, 38, 2.3, hoursOperation, salesAmounts, totalSales);
-var alki = new Stores('alki', 2, 16, 4.6, hoursOperation, salesAmounts, totalSales);
+var pike = new Stores('pike', 23, 65, 6.3);
+pike.hourlySales();
+pike.rows();
+
+var seaTac = new Stores('seaTac', 3, 24, 1.2);
+seaTac.hourlySales();
+seaTac.rows();
+
+var seaCenter = new Stores('seaCenter', 11, 38, 3.7);
+seaCenter.hourlySales();
+seaCenter.rows();
+
+var capHill = new Stores('capHill', 20, 38, 2.3);
+capHill.hourlySales();
+capHill.rows();
+
+var alki = new Stores('alki', 2, 16, 4.6);
+alki.hourlySales();
+alki.rows();
 
 //Push new objects into the stores array
+var store = [];
+
 store.push(pike);
 store.push(seaTac);
 store.push(seaCenter);
 store.push(capHill);
 store.push(alki);
-
-
-
-
-
-
-
-
-//push store objects into data array
-for (var i = 0 ; i < stores.length ; i++){
-  data.push(
-    '<td>' + stores[i].minCustomers + '</td>' +
-    '<td>' + stores[i].maxCustomers + '</td>' +
-    '<td>' + stores[i].avg + '</td>' +
-    '<td>' + stores[i].salesAmounts + '</td>'
-  );
-}
-
-var table = document.getElementById('table_content');
-var data = [];
-
-//Creating Table Rows and Appending to HTML
-var tableRow;
-
-for (var l = 0 ; l < salesAmounts.length ; l++){
-  data.push('<th>' + salesAmounts[l] + '</th>');
-}
-
-for (var m = 0 ; m < data.length ; m++){
-  tableRow = document.createElement('tr');
-  tableRow.innerHTML = data[j];
-  table.appendChild(tableRow);
-}
-//rg
